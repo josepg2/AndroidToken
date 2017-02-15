0f94b0f54ca02686e5c3662366bab77818171560
josepg2 <georgejoseph2sep92@gmail.com>
2017-02-15 23:49:10 +0530

    Added helper db class

--- src/main/java/com/embeddedproject/projecttoken/myapplication/DBHelper.java
+++ src/main/java/com/embeddedproject/projecttoken/myapplication/DBHelper.java
@@ -1,8 +1,34 @@
 package com.embeddedproject.projecttoken.myapplication;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by josepg4 on 13/2/17.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "unattended.db";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table unAttentedTokens" +
+                        "(_id integer primary key, name text,phone text,email text, street text,place text)"
+        );
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
 }
